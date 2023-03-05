f254fdadcff715a13512d558bd7385d4e3b82046
David <dong05625@gmail.com>
2023-03-05 23:42:10 +0800

    创建子应用

--- bookmanager/bookmanager/settings.py
+++ bookmanager/bookmanager/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
+# 注册子应用
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 'book', # method 1
+    'book.apps.BookConfig', # method 2
 ]
 
 MIDDLEWARE = [
